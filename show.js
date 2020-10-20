a7edffa3402b2e3d30080226328ba8bc924508e2
Julalak <s6030213020@phuket.psu.ac.th>
2020-10-20 20:18:24 +0700

    edit prduct.html

--- show.js
+++ show.js
@@ -24,18 +24,3 @@ $(document).ready(function() {
         document.getElementById('thumbnails').innerHTML = show;
     });
 });
-// $(document).ready(function() {
-//   var url = "http://localhost:3000/product?";
-//   $.get(url, function(data) {
-//     //  console.log(data);          
-//       var all =data.length;
-//       // var r ="";
-//       // for(var i =0;i<all;i++){
-//       //     r += '<tr><td><a href='+'"custdetail.html?id='+data[i].customerID+'"'+">"+data[i].customerID+'</a></td><td>'
-//       //  +data[i].companyName+'</td><td>'+data[i].contactName+'</td><td>'+data[i].contactTitle+'</td></tr>';                   
-//       // }  
-//       console.log(all);
-//       //$('#totalProduct').append(all);
-//       document.getElementById('totalProduct').innerHTML = all;
-//   });
-// });
