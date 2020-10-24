caa077d2f3ed8bf4c4b90f924b4baefae70d174d
MP_Praewrung <38246225+meipraewrung@users.noreply.github.com>
2020-10-24 18:12:53 +0700

    back

--- admin/products.js
+++ admin/products.js
@@ -1,38 +1,3 @@
-//product listView
-$(document).ready(function() {
-  var url = "http://localhost:3000/product?";
-  $.get(url, function(data) {
-    console.log(data);          
-    var all = data.length;
-    var showList ="";
-    for(var i=1 ;i<=all ;i++){
-      showList += 
-      '<div class="row">'+
-        '<div class="span2">'+
-          '<img style="height: 150px; width: auto" src="'+data[i].proImage+'" alt="" />'+
-        '</div>'+
-        '<div class="span4">'+
-          '<h3>'+ data[i].proName+'</h3>'+
-          '<hr class="soft" />'+
-          '<h5>'+ data[i].proName+'</h5>'+
-          '<p>'+ data[i].proName+'</p>'+
-          '<a class="btn btn-small pull-right" href="productsDetails.html?productID='+i+'">รายละเอียด'+
-            '<i class=""></i></a>'+
-          '<hr class="soft" />'+
-        '</div>'+
-        '<div class="span3 alignR">'+
-          '<form class="form-horizontal qtyFrm">'+
-            '<h3>฿'+data[i].priceUnit+'</h3>'+
-            '<a class="btn btn-primary" href="productsEdit.html?productID='+i+'">แก้ไข'+
-              '<i class=""></i></a> '+
-            '<a class="btn btn-danger" href="products.html?productID='+i+'">ลบ'+
-              '<i class=""></i></a>'+
-          '<form>'+'</div>'+'</div>'+
-          '<hr class="soft" />';
-        }
-    document.getElementById('proListView').innerHTML = showList;
-  });
-});
 
 //product blockView
 $(document).ready(function() {
@@ -41,15 +6,7 @@ $(document).ready(function() {
     //  console.log(data);          
       var all =data.length;
       var show ="";
-<<<<<<< HEAD
-<<<<<<< HEAD
-      for(var i=1 ;i<=all ;i++){
-=======
       for(var i =0;i<all;i++){
->>>>>>> parent of 3060391... edit All link , move file and write get by id in product_details for admin
-=======
-      for(var i =0;i<all;i++){
->>>>>>> parent of 3060391... edit All link , move file and write get by id in product_details for admin
        show+= 
        '<li class="span3">'+
           '<div class="thumbnail">'+
@@ -59,12 +16,12 @@ $(document).ready(function() {
               '<div class="caption">'+
                 '<h5>'+ data[i].proName+'</h5>'+
                 '<h4 style="text-align:center">'+
-                  '<a class="btn" href="product_details.html?productID='+i+'">'+
-                    '<i class="icon-zoom-in"></i></a> '+
-                  '<a class="btn btn-primary" href="productsEdit.html?productID='+i+'">แก้ไข'+
-                    '<i class=""></i></a> '+
-                  '<a class="btn btn-danger" href="products.html?productID='+i+'">ลบ'+
-                    '<i class=""></i></a>'+
+                '<a class="btn" href="product_details.html?productID='+i+'">'+
+                  '<i class="icon-zoom-in"></i></a> '+
+                '<a class="btn btn-primary" href="productsEdit.html?productID='+i+'">แก้ไข'+
+                  '<i class=""></i></a> '+
+                '<a class="btn btn-danger" href="products.html?productID='+i+'">ลบ'+
+                  '<i class=""></i></a>'+
                 '</h4>'+
               '</div>'+'</div>'+'</li>';
       }
