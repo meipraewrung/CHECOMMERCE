99c42ead0e2590cf9da8b7085207447b5259384d
JulalakTammaphet <48485581+JulalakTammaphet@users.noreply.github.com>
2020-10-28 02:07:25 +0700

    เเก้ไขหน้าลงทะเบียน+user.json+user.js

---
+++ js/jsUsers/register.js
@@ -0,0 +1,34 @@
+$("#register").click(function () {
+    console.log('Register');
+    let newuser = {};
+    // newuser.id = pid;
+    newuser.Title = $("#Title").val();
+    newuser.FristName = $("#inputFname1").val();
+    newuser.LastName = $("#inputLname").val();
+    newuser.Email = $("#input_email").val();
+    newuser.Password = $("#inputPassword1").val();
+    newuser.DateofBirth = $("#bday").val();
+    newuser.Address = $("#address").val();
+    newuser.PostalCode = $("#postcode").val();
+    newuser.MobilePhone = $("#mobilephone").val();
+    
+
+
+    
+
+    console.log(newuser);
+    $.ajax({
+        url: "http://localhost:3000/user/",
+        type: 'POST',
+        // data: newuser,
+        dataType: 'json',
+        contentType: 'application/json',
+        data: JSON.stringify(newuser),
+        success: function (result) {
+            console.log('Success new user!');
+        }
+    });
+    $("#err").show();
+    //window.open('register.html');
+    
+});
