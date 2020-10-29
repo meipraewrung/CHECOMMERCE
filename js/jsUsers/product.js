54f2be7da0532d5470c8701279db22387da0fdf3
MP_Praewrung <38246225+meipraewrung@users.noreply.github.com>
2020-10-30 01:34:07 +0700

    Show Case

--- js/jsUsers/product.js
+++ js/jsUsers/product.js
@@ -6,10 +6,10 @@ $(document).ready(function() {
 	  console.log('data',data);          
 		var all =data.length;
 		let show = "";
-		for (let index = 0; index < data.length; index++) {
+		for (let index = 0; index < 18; index++) {
 		  show+= 
 		  '<li class="span3">'+
-		  	'<div class="thumbnail">'+
+		  	'<div class="thumbnail" style="height: 350px;>'+
 		  		'<a href="product_details.html?productID='+data[index].id+'">'+
 					'<img style="height: 150px; width: auto" src="'+data[index].proImage+'" alt="" />'+
 		  		'</a>'+
@@ -19,7 +19,9 @@ $(document).ready(function() {
 					'<h4 style="text-align:center">'+
 					'<a class="btn" href="product_details.html?productID='+(index+1)+'">'+
 						'<i class="icon-zoom-in"></i></a> '+
-					'<a class="btn" href="product_summary.html?productID='+(index+1)+'">'+'เพิ่ม'+
+					// '<a class="btn" href="product_summary.html?productID='+(index+1)+'">'+'เพิ่ม'+
+					// 	'<i class="icon-shopping-cart"></i></a> '+ 
+					'<a class="btn" href="#">'+'เพิ่ม'+
 						'<i class="icon-shopping-cart"></i></a> '+ 
 					'<a class="btn btn-primary" href="#">฿'+data[index].priceUnit+'</a></h4>'+
 		 		 '</div>'+'</div>'+'</li>';
@@ -28,6 +30,76 @@ $(document).ready(function() {
 		}
 		// console.log('show',show);
 		document.getElementById('showProduct').innerHTML = show;
+	});
+  });
+
+  //product blockView
+$(document).ready(function() {
+	var url = "http://localhost:3000/product?";
+	console.log(url);
+	$.get(url, function(data) {
+	  console.log('data',data);          
+		var all =data.length;
+		let show = "";
+		for (let index = 19; index < 37; index++) {
+		  show+= 
+		  '<li class="span3">'+
+		  	'<div class="thumbnail" style="height: 350px;>'+
+		  		'<a href="product_details.html?productID='+data[index].id+'">'+
+					'<img style="height: 150px; width: auto" src="'+data[index].proImage+'" alt="" />'+
+		  		'</a>'+
+		  		'<div class="caption">'+
+					'<h5>'+ JSON.stringify(data[index].proName)+'</h5>'+
+					'<p>'+ data[index].priceUnit+'</p>'+
+					'<h4 style="text-align:center">'+
+					'<a class="btn" href="product_details.html?productID='+(index+1)+'">'+
+						'<i class="icon-zoom-in"></i></a> '+
+					// '<a class="btn" href="product_summary.html?productID='+(index+1)+'">'+'เพิ่ม'+
+					// 	'<i class="icon-shopping-cart"></i></a> '+ 
+					'<a class="btn" href="#">'+'เพิ่ม'+
+						'<i class="icon-shopping-cart"></i></a> '+ 
+					'<a class="btn btn-primary" href="#">฿'+data[index].priceUnit+'</a></h4>'+
+		 		 '</div>'+'</div>'+'</li>';
+		  // console.log('show',show);
+		  
+		}
+		// console.log('show',show);
+		document.getElementById('showProduct2').innerHTML = show;
+	});
+  });
+
+  //product blockView
+$(document).ready(function() {
+	var url = "http://localhost:3000/product?";
+	console.log(url);
+	$.get(url, function(data) {
+	  console.log('data',data);          
+		var all =data.length;
+		let show = "";
+		for (let index = 38; index < 57; index++) {
+		  show+= 
+		  '<li class="span3">'+
+		  	'<div class="thumbnail" style="height: 350px;>'+
+		  		'<a href="product_details.html?productID='+data[index].id+'">'+
+					'<img style="height: 150px; width: auto" src="'+data[index].proImage+'" alt="" />'+
+		  		'</a>'+
+		  		'<div class="caption">'+
+					'<h5>'+ JSON.stringify(data[index].proName)+'</h5>'+
+					'<p>'+ data[index].priceUnit+'</p>'+
+					'<h4 style="text-align:center">'+
+					'<a class="btn" href="product_details.html?productID='+(index+1)+'">'+
+						'<i class="icon-zoom-in"></i></a> '+
+					// '<a class="btn" href="product_summary.html?productID='+(index+1)+'">'+'เพิ่ม'+
+					// 	'<i class="icon-shopping-cart"></i></a> '+ 
+					'<a class="btn" href="#">'+'เพิ่ม'+
+						'<i class="icon-shopping-cart"></i></a> '+ 
+					'<a class="btn btn-primary" href="#">฿'+data[index].priceUnit+'</a></h4>'+
+		 		 '</div>'+'</div>'+'</li>';
+		  // console.log('show',show);
+		  
+		}
+		// console.log('show',show);
+		document.getElementById('showProduct3').innerHTML = show;
 	});
   });
 
